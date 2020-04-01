f58fd0c9d94c3b9624b8b9b528609dbfb9c1c3b0
Markus Sveggen <marks18@uia.no>
2020-04-01 19:29:57 +0200

    minor optimalizations

---
+++ app/src/main/java/org/wit/rightcard/AddCardActivity.kt
@@ -0,0 +1,12 @@
+package org.wit.rightcard
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCardActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_card)
+    }
+}
--- app/src/main/java/org/wit/rightcard/MainActivity.kt
+++ app/src/main/java/org/wit/rightcard/MainActivity.kt
@@ -2,6 +2,7 @@ package org.wit.rightcard
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import android.view.*
 import kotlinx.android.synthetic.main.activity_main.*
 import org.wit.rightcard.R
@@ -11,11 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-      // setSupportActionBar(findViewById(R.id.toolbar))
-
-        toolbar.title = title
-        setSupportActionBar(toolbar)
-
+        setSupportActionBar(findViewById(R.id.toolbar))
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
