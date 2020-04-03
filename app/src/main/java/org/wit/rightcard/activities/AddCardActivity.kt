e448e1c397bd94917e32efdbe710637e4adfdc1f
Markus Sveggen <marks18@uia.no>
2020-04-03 16:46:10 +0200

    created spinner with dummy items

--- app/src/main/java/org/wit/rightcard/activities/AddCardActivity.kt
+++ app/src/main/java/org/wit/rightcard/activities/AddCardActivity.kt
@@ -4,19 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import kotlinx.android.synthetic.main.activity_add_card.*
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import org.jetbrains.anko.startActivityForResult
 import org.wit.rightcard.R
 
-class AddCardActivity : AppCompatActivity(), AnkoLogger {
+class AddCardActivity : AppCompatActivity(), AnkoLogger, AdapterView.OnItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_card)
         //Init toolbar
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+        // ArrayAdapter using the dummy string array
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.CreditCards,
+            android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Applies the adapter to the spinner
+            spinner.adapter = adapter
+        }
         //Log to console when card is added
         btnAddCard.setOnClickListener(){
             info("Card added")
@@ -33,6 +49,16 @@ class AddCardActivity : AppCompatActivity(), AnkoLogger {
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+
+    }
+
+    override fun onItemSelected(parent:AdapterView<*>, view:View, pos:Int, id:Long) {
+      val spinneritem = parent.getItemAtPosition(pos)
+        //get id of item
+        //save id to users to json
     }
 
 }
